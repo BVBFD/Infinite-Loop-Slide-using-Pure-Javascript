6d7ca84ffa80caca9bc2ea17dcd2c76f514cd540
BVBFD <lsevina126@gmail.com>
2021-12-12 10:18:10 +0900

    Infinite Loop Slide using Pure Javascript

---
+++ script.js
@@ -0,0 +1,68 @@
+"use strict";
+
+let slides = document.querySelector(".slides"),
+  slide = document.querySelectorAll(".slides li"),
+  currentIdx = 0,
+  slideCount = slide.length,
+  slideWidth = 200,
+  slideMargin = 30,
+  prevBtn = document.querySelector(".prev"),
+  nextBtn = document.querySelector(".next");
+
+const moveSlide = (num) => {
+  slides.style.left = `${-num * (slideWidth + slideMargin)}px`;
+  currentIdx = num;
+  if (currentIdx == slideCount || currentIdx == -slideCount) {
+    setTimeout(() => {
+      slides.classList.remove("animated");
+      slides.style.left = "0px";
+      currentIdx = 0;
+    }, 500);
+    setTimeout(() => {
+      slides.classList.add("animated");
+    }, 600);
+  }
+};
+
+nextBtn.addEventListener("click", () => {
+  moveSlide(currentIdx + 1);
+});
+
+prevBtn.addEventListener("click", () => {
+  moveSlide(currentIdx - 1);
+});
+
+const setInitialPos = () => {
+  let initialTranslateValue = -(slideWidth + slideMargin) * slideCount;
+  slides.style.transform = `translateX(${initialTranslateValue}px)`;
+};
+
+const updateWidth = () => {
+  let currentSlides = document.querySelectorAll(".slides li");
+  let newSlideCount = currentSlides.length;
+  let newWidth =
+    (slideWidth + slideMargin) * newSlideCount - slideMargin + "px";
+  slides.style.width = newWidth;
+};
+
+const makeClone = () => {
+  for (let i = 0; i < slideCount; i++) {
+    let cloneSlide = slide[i].cloneNode(true);
+    cloneSlide.classList.add("clone");
+    slides.appendChild(cloneSlide);
+  }
+
+  for (let i = slideCount - 1; i >= 0; i--) {
+    let cloneSlide = slide[i].cloneNode(true);
+    cloneSlide.classList.add("clone");
+    slides.prepend(cloneSlide);
+  }
+
+  updateWidth();
+  setInitialPos();
+  setTimeout(() => {
+    slides.classList.add("animated");
+  }, 100);
+};
+
+makeClone();
